7670de1376bef4b81a89142abff6dfa1426b5329
Mario Bergeron <grouby177@gmail.com>
2025-06-14 14:15:51 -0400

    ros2: fix handling of model_path/model_name parameters in launch files.

--- ros2_ws/asl_mediapipe_pointnet/launch/asl_mediapipe_pointnet_turtlesim_launch.py
+++ ros2_ws/asl_mediapipe_pointnet/launch/asl_mediapipe_pointnet_turtlesim_launch.py
@@ -1,8 +1,20 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "model_path",
+            default_value="/media/albertabeef/Tycho/asl_mediapipe_pointnet/model",
+            description="Path (absolute) to PointNet model."
+        ),
+        DeclareLaunchArgument(
+            "model_name",
+            default_value="point_net_1.pth",
+            description="Name of PointNet model."
+        ),
         Node(
             package='v4l2_camera',
             executable='v4l2_camera_node',
@@ -13,7 +25,7 @@ def generate_launch_description():
             package='asl_mediapipe_pointnet',
             executable='asl_mediapipe_pointnet_demo',
             name="my_asl_controller",
-            parameters=[{"model_path","/media/albertabeef/Tycho/asl_mediapipe_pointnet/model"},{"model_name","point_net_1.pth"}],
+            parameters=[{"model_path":LaunchConfiguration("model_path")},{"model_name":LaunchConfiguration("model_name")}],
             remappings=[("image_raw", "my_input_image"),("cmd_vel", "turtle1/cmd_vel")]            
         ),
         Node(
